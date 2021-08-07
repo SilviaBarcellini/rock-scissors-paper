bbb1e57f35c01b2ebc9478b9262f4b4cd8141890
Your Name <barcellinisilvia@gmail.com>
2021-08-07 17:51:56 +0200

    ✨ add user choices

--- app.js
+++ app.js
@@ -1,7 +1,28 @@
 const startGameBtn = document.getElementById("start-game-btn")
 
-function startGame() {
-    console.log("Starting game...");
+const ROCK = "ROCK"; 
+const SCISSORS = "SCISSORS";
+const PAPER = "PAPER";
+const DEFAULT_CHOICE = ROCK;
+
+const playerChoice = function() {
+    const selection = prompt(`Hi! Are you ${ROCK}, ${SCISSORS} or ${PAPER}?`, "").toUpperCase();
+    if (
+        selection !==  ROCK &&
+        selection !==  SCISSORS &&
+        selection !==  PAPER
+    ) {
+        alert(`Invalid enter! I'll choose ${DEFAULT_CHOICE} for you!`);
+        return DEFAULT_CHOICE;
+    }
+    return selection;
 }
 
-startGameBtn.addEventListener("click", startGame);
+
+startGameBtn.addEventListener("click", function() {
+    console.log("Starting game...");
+    const playerSelection = playerChoice(); 
+    console.log(playerSelection);
+});
+
+//a method is a function stored inside an object
