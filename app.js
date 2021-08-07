84c5b065062b60155959b5a1f81ecad3ec7523c9
Your Name <barcellinisilvia@gmail.com>
2021-08-07 16:30:48 +0200

    ✨ add event listener to btn

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
 const startGameBtn = document.getElementById("start-game-btn")
+
+function startGame() {
+    console.log("Starting game...");
+}
+
+startGameBtn.addEventListener("click", startGame);
